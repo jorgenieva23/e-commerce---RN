a071813fc93011f4393d0d16d619584a173bb7ef
jorgenieva23 <jorge_4755@hotmail.com>
2025-06-11 19:30:00 -0300

    product

--- src/presentation/components/products/ProductCard.tsx
+++ src/presentation/components/products/ProductCard.tsx
@@ -1,32 +1,40 @@
 import { Image } from 'react-native';
 import React from 'react';
 import { Product } from '../../../domain/entities/product';
-import { Card } from '@ui-kitten/components';
-import { FadeInImage } from '../ui/FadeInImage';
+import { Card, Text } from '@ui-kitten/components';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../navigation/StackNavigator';
 
 interface Props {
   product: Product;
 }
 
 export const ProductCard = ({ product }: Props) => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
   return (
     <Card
+      onPress={() =>
+        navigation.navigate('ProductScreen', { productId: product.id })
+      }
       style={{
         flex: 1,
         backgroundColor: '#F9F9F9',
         margin: 3,
       }}>
       {product.images.length === 0 ? (
-        <Image
-          source={require('../../../assets/no-product-image.png')}
-          style={{ width: '100%', height: '100%' }}
-        />
+        <Text>sin imagen</Text>
       ) : (
-        <FadeInImage
-          uri={product.images[0]}
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        />
+        <Text>{product.id}</Text>
+        // <Image
+        //   source={{ uri: product.images[0] }}
+        //   style={{ width: 100, height: 100 }}
+        // />
       )}
+      <Text numberOfLines={2} style={{ textAlign: 'center' }}>
+        {product.title}
+      </Text>
     </Card>
   );
 };
+// GIT_AUTHOR_DATE="2024-06-03T10:00:00"GIT_COMMITTER_DATE"2024-06-03T10:00:00"git commit -m "descripcion"
