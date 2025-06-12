2ac18ca9f588cdc58f2497f00882e7e187b34b7a
jorgenieva23 <jorge_4755@hotmail.com>
2025-06-12 19:30:00 -0300

    product

--- src/presentation/components/products/ProductsList.tsx
+++ src/presentation/components/products/ProductsList.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, List } from '@ui-kitten/components';
 import { Product } from '../../../domain/entities/product';
 import { ProductCard } from './ProductCard';
+import { RefreshControl } from 'react-native-gesture-handler';
 
 interface Props {
   products: Product[];
+  fetchNextPage: () => void;
 }
 
-export const ProductsList = ({ products }: Props) => {
+export const ProductsList = ({ products, fetchNextPage }: Props) => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onPullToRefresh = async () => {
+    setIsRefreshing(true);
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    setIsRefreshing(false);
+  };
+
   return (
     <List
       data={products}
@@ -15,6 +25,11 @@ export const ProductsList = ({ products }: Props) => {
       keyExtractor={(item, index) => `${item.id}-${index}`}
       renderItem={({ item }) => <ProductCard product={item} />}
       ListFooterComponent={() => <Layout style={{ height: 150 }} />}
+      onEndReached={fetchNextPage}
+      onEndReachedThreshold={0.8}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onPullToRefresh} />
+      }
     />
   );
 };
