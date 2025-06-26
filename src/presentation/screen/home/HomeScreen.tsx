abca1aee3be37210721ad3a88a57a9900038e845
jorgenieva23 <jorge_4755@hotmail.com>
2025-06-26 18:00:00 -0300

    Home

--- src/presentation/screen/home/HomeScreen.tsx
+++ src/presentation/screen/home/HomeScreen.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Icon } from '@ui-kitten/components';
-import { getProductsByPage } from '../../../actions/products/get-products-by-page';
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+
 import { MainLayout } from '../../layouts/MainLayout';
 import { FullScreenLouder } from '../../components/ui/FullScreenLouder';
 import { ProductsList } from '../../components/products/ProductsList';
+import { FAB } from '../../components/ui/FAB';
+import { RootStackParams } from '../../navigation/StackNavigator';
+import { getProductsByPage } from '../../../actions/products/get-products-by-page';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
   const { isLoading, data, fetchNextPage } = useInfiniteQuery({
     queryKey: ['products', 'infinite'],
     staleTime: 1000 * 60 * 60, // 1 hour
@@ -17,19 +22,32 @@ export const HomeScreen = () => {
   });
 
   return (
-    <MainLayout
-      title="TesloShop - Products"
-      subTitle="Aplicación administrativa"
-      rightAction={() => {}}
-      rightActionIcon="shopping-cart-outline">
-      {isLoading ? (
-        <FullScreenLouder />
-      ) : (
-        <ProductsList
-          products={data?.pages.flat() ?? []}
-          fetchNextPage={fetchNextPage}
-        />
-      )}
-    </MainLayout>
+    <>
+      <MainLayout
+        title="TesloShop - Products"
+        subTitle="Aplicación administrativa"
+        rightAction={() => {}}
+        rightActionIcon="shopping-cart-outline">
+        {isLoading ? (
+          <FullScreenLouder />
+        ) : (
+          <ProductsList
+            products={data?.pages.flat() ?? []}
+            fetchNextPage={fetchNextPage}
+          />
+        )}
+      </MainLayout>
+      <FAB
+        iconName="plus-outline"
+        onPress={() =>
+          navigation.navigate('ProductScreen', { productId: 'new' })
+        }
+        style={{
+          position: 'absolute',
+          bottom: 30,
+          right: 20,
+        }}
+      />
+    </>
   );
 };
