55672afcc11eec2c52bf654b4d7caa4695d3552f
jorgenieva23 <jorge_4755@hotmail.com>
2025-06-27 12:00:00 -0300

    Produclist

--- src/actions/products/get-product-by-id.ts
+++ src/actions/products/get-product-by-id.ts
@@ -1,9 +1,23 @@
 import { tesloApi } from '../../configs/api/telsloApi';
-import { Product } from '../../domain/entities/product';
+import { Gender, Product } from '../../domain/entities/product';
 import { TesloProduct } from '../../infrastructure/interface/teslo-products-response';
 import { ProductMapper } from '../../infrastructure/mappers/product-mappers';
 
+const emptyProduct: Product = {
+  id: '',
+  title: 'Nuevo producto',
+  description: '',
+  price: 0,
+  images: [],
+  slug: '',
+  gender: Gender.Unisex,
+  sizes: [],
+  stock: 0,
+  tags: [],
+};
+
 export const getProductById = async (id: string): Promise<Product> => {
+  if (id === 'new') return emptyProduct;
   try {
     const { data } = await tesloApi.get<TesloProduct>(`/products/${id}`);
     return ProductMapper.tesloProductToEntity(data);
