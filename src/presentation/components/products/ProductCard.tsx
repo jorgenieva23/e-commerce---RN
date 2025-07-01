161484723ad6440a2fe36627565d0ca5503e6498
jorgenieva23 <jorge_4755@hotmail.com>
2025-07-01 12:00:00 -0300

    API

--- src/presentation/components/products/ProductCard.tsx
+++ src/presentation/components/products/ProductCard.tsx
@@ -21,17 +21,17 @@ export const ProductCard = ({ product }: Props) => {
       style={{
         flex: 1,
         backgroundColor: '#F9F9F9',
-        margin: 3,
+        margin: 1,
       }}>
       {product.images.length === 0 ? (
         <Image
           source={require('../../../assets/no-product-image.png')}
-          style={{ width: 200, height: 200 }}
+          style={{ width: '100%', height: 200 }}
         />
       ) : (
         <FadeInImage
           uri={product.images[0]}
-          style={{ flex: 1, height: 200, width: 200 }}
+          style={{ flex: 1, height: 200, width: 190 }}
         />
       )}
       <Text numberOfLines={2} category="s1" style={{ textAlign: 'left' }}>
